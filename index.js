7e0e130adc38f6fca0e03780f91b7c72f322e659
jedrek89 <marcinj3@gmail.com>
2022-12-31 19:31:37 +0100

    add slider

--- index.js
+++ index.js
@@ -1,5 +1,121 @@
-console.log('js works!!!')
-
-
 // date for footer
 document.getElementById("year").innerHTML = new Date().getFullYear() + " ";
+
+let slideStatus;
+slideStatus = 1;
+
+document.getElementById('headerLogo').addEventListener('click', function() {
+    slideStatus = 1;
+    removeClasses();
+    document.getElementById('aboutMe').classList.add('liActive');
+    slide1();
+    console.log("slideStatus", slideStatus);
+})
+
+
+// Slide 1
+document.getElementById('aboutMe').addEventListener('click', function() {
+    slideStatus = 1;
+    removeClasses();
+    document.getElementById('aboutMe').classList.add('liActive');
+    slide1();
+    console.log("slideStatus", slideStatus);
+})
+
+// Slide 2
+document.getElementById('hardware').addEventListener('click', function() {
+    slideStatus = 2;
+    removeClasses();
+    document.getElementById('hardware').classList.add('liActive');
+    slide2();
+    console.log("slideStatus", slideStatus);
+})
+
+// Slide 3
+document.getElementById('networks').addEventListener('click', function() {
+    slideStatus = 3;
+    removeClasses();
+    document.getElementById('networks').classList.add('liActive');
+    slide3();
+    console.log("slideStatus", slideStatus);
+})
+
+// Slide 4
+document.getElementById('services').addEventListener('click', function() {
+    slideStatus = 4;
+    removeClasses();
+    document.getElementById('services').classList.add('liActive');
+    slide4();
+    console.log("slideStatus", slideStatus);
+})
+
+// Slide 5
+document.getElementById('contact').addEventListener('click', function() {
+    slideStatus = 5; 
+    removeClasses();
+    document.getElementById('contact').classList.add('liActive');
+    slide5();
+    console.log("slideStatus", slideStatus);
+})
+
+
+// Remove slides / classes
+function removeClasses() {
+    document.getElementById('aboutMe').classList.remove('liActive');
+    document.getElementById('hardware').classList.remove('liActive');
+    document.getElementById('networks').classList.remove('liActive');
+    document.getElementById('services').classList.remove('liActive');
+    document.getElementById('contact').classList.remove('liActive');
+}
+
+function slide1() {
+    hideSlides();
+    document.getElementById('sliderBackground1').classList.add('sliderBackground1');
+}
+
+function slide2() {
+    hideSlides();
+    document.getElementById('sliderBackground2').classList.add('sliderBackground2');
+}
+
+function slide3() {
+    hideSlides();
+    document.getElementById('sliderBackground3').classList.add('sliderBackground3');
+}
+
+function slide4() {
+    hideSlides();
+    document.getElementById('sliderBackground4').classList.add('sliderBackground4');
+}
+
+function slide5() {
+    hideSlides();
+    document.getElementById('sliderBackground5').classList.add('sliderBackground5');
+}
+
+
+function hideSlides() {
+    document.getElementById('sliderBackground1').classList.remove('sliderBackground1');
+    document.getElementById('sliderBackground1').classList.add('hideItem');
+    document.getElementById('sliderBackground2').classList.remove('sliderBackground2');
+    document.getElementById('sliderBackground2').classList.add('hideItem');
+    document.getElementById('sliderBackground3').classList.remove('sliderBackground3');
+    document.getElementById('sliderBackground3').classList.add('hideItem');
+    document.getElementById('sliderBackground4').classList.remove('sliderBackground4');
+    document.getElementById('sliderBackground4').classList.add('hideItem');
+    document.getElementById('sliderBackground5').classList.remove('sliderBackground5');
+    document.getElementById('sliderBackground5').classList.add('hideItem');
+}
+
+
+
+
+// Previous slide
+document.getElementById('leftArrowSlider').addEventListener('click', function() {
+
+})
+
+// Next slide
+document.getElementById('rightArrowSlider').addEventListener('click', function() {
+    console.log("rightArrowSlider clicked!!")
+})
